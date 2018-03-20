7540621a3c93809bebff54a504c54df3814e3823
Jacques Bellisario <gypsyoak@yahoo.com>
2018-03-20 05:22:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+function logShout(string) {
+  return console.log(string.toUpperCase())
+}
+function logWhisper(string) {
+  return console.log(string.toLowerCase())
+}
+sayHitoGrandma(string) {
+  if (string.toUpperCase() === string)
+  console.log("YES INDEED!")
+  else (string.toLowerCase() === string)
+  console.log("I can't hear you!")
+}
